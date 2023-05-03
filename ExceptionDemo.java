2d80f68242534ef8049899d67da3f79c1735935c
sandeep010169191 <Sandeep.Chakraborty1@gds.ey.com>
2023-05-03 15:11:28 +0530

    ExceptionDemo.java

---
+++ ExceptionDemo.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class ExceptionDemo {
+    public static void main(String[] args) {
+        try {
+            int i = 5;
+            int j = 0;
+            System.out.println("Result: " + (i / j));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        int arr[] = new int[3];
+        try {
+            Scanner sc = new Scanner(System.in);
+            for (int i = 0; i <= arr.length; i++) {
+                arr[i] = sc.nextInt();
+            }
+            System.out.println(arr);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        Integer i = null;
+        try {
+            int j = i.parseInt("sandeep");
+            System.out.println(j);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
